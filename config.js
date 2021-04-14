b110ce7770748815f9cf0364047b93acfbd3f900
Mlbn <jojjemellbin@gmail.com>
2021-04-14 22:27:12 +0200

    .

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
 // @ts-check
 
-const config = {
-    endpoint: "https://p3kkan.documents.azure.com:443/",
-    key: "zrcZZTlBsQJrMaSXx4lCfIXBHGjGVeRgEYWy4SHGTHnIlVdO6JSYDXM7ea5nbIETa0GzgMmvmweBseAsZUmECA==",
-    databaseId: "P3kkan",
-    containerId: "bangers",
-    partitionKey: { kind: "Hash", paths: ["/bangers"] }
-  };
-  
+const config  = {
+  dcToken: "ToBeAdded"
+}
   
   module.exports = config;
