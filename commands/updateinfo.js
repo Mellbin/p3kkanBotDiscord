f465caef298a03fc24da1df5d29922acd6b25498
Mlbn <jojjemellbin@gmail.com>
2021-04-15 22:15:53 +0200

    Added alot of shit

---
+++ commands/updateinfo.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: `updateinfo`,
+    description: "uppdaterar dc",
+    execute(message, args){
+        try{
+            client.user.setActivity("Handlar järnrör på svarta marknaren");
+            client.user.setUsername("Diktatorn's au pair")
+            client.user.setAvatar("https://c.stocksy.com/a/jq3800/z9/1921425.jpg")
+          }
+            catch(error){
+              console.log(error) //Hella not working idk why, den fungerar även inte i externt
+            } 
+    }
+};
--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
-const DisTube = require("distube")
 const sql = require("mySql");
 const { dcToken } = require('./config');
 const config = require('./config');
-const fs = require("fs.js");
+const fs = require (`fs`);
+
+const DisTube = require("distube");
+const play = require('./commands/play');
 
 const client = new Discord.Client();
 const distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true });
 const prefix = "!";
 
-const fs = require (`fs`);
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith(`.js`));
 for(const file of commandFiles){
@@ -17,6 +19,12 @@ for(const file of commandFiles){
 
     client.commands.set(command.name, command);
 }
+
+client.login(config.dcToken);
+client.once("ready", () => {
+    console.log("pekkan is ready");  
+});
+
 
 function randomInt(low, high) { //funktion för val av banger
   return Math.floor(Math.random() * (high - low) + low)
@@ -51,12 +59,6 @@ distube
       });
   });
 
-client.login(config.dcToken);
-client.once("ready", () => {
-    
-    
-    console.log("pekkan is ready");  
-});
 
 client.on (`message`, message =>{
   if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -71,66 +73,7 @@ client.on (`message`, message =>{
     console.log(error)
   }
 
-    if (command === "play"){
-      try {
-        distube.play(message, args.join(" "));
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    else if (command === "stop"){
-      distube.stop(message);
-      message.channel.send("music go stop").then(message =>{
-        message.delete({ timeout: 20000})
-      })
-    
-    } 
-    else if (command == "skip"){
-        distube.skip(message);
-      }
-    else if (command == "queue") {
-      try {
-        let queue = distube.getQueue(message);
-        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
-            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
-        ).slice(0, 10).join("\n")).then(message =>{
-          message.delete({ timeout: 20000})
-        })
-      } catch (error) {
-        console.log(error);
-      }
-      
-    }
+  });
 
-    //music command2
-    else if(command === "banger"){
-        if(randomInt(1, 5) === 1){
-            message.channel.send("https://www.youtube.com/watch?v=fH1PSDZOtn0");
-        }
-        else if (randomInt(1,5) === 2){
-            message.channel.send("https://www.youtube.com/watch?v=w2IhccXakkE");
-        }
-        else if (randomInt(1,5) === 3){
-        message.channel.send("https://www.youtube.com/watch?v=cCyDTsR3Z-8");
-        }
-        else if (randomInt(1,5) === 4){
-        message.channel.send("https://www.youtube.com/watch?v=_L2vJEb6lVE");
-        }
-        else if(randomInt(1,5) === 5){
-            message.channel.send("https://www.youtube.com/watch?v=eCGV26aj-mM");
-            }
-
-      else if(command === "updateinfo"){
-        try{
-        client.user.setActivity("Handlar järnrör på svarta marknaren");
-        client.user.setUsername("Diktatorn's au pair")
-        client.user.setAvatar("https://c.stocksy.com/a/jq3800/z9/1921425.jpg")
-      }
-        catch(error){
-          console.log(error) //Hella not working idk why, den fungerar även inte i externt
-        } 
-    }
-  }    
-})
-
-client.login(`ODMxOTgzMTY5MDQ0NzQyMjI0.YHdKtQ._b3xPekmRpWCsqTUm3uZpzyThuE`);
+module.exports = distube;
+module.exports = client;
